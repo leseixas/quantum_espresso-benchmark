32a05f01e3c319befac52b7460bb6ccdeaa66ed3
Leandro Seixas <seixasle@gmail.com>
2015-12-14 04:34:37 +0800

    Create inputs and scripts to Graphene/basis/RRKJUS

---
+++ Graphene/basis/RRKJUS/run_espresso.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for rho in $(seq 4 2 12)
+  do
+  cd rho_$rho
+  for wfc in $(seq 20 5 150)
+  do
+    cd wfc_$wfc
+    mpiexec pw.x -npool 2 -inp pw.relax.espresso > pw.relax.out
+    rm -rf pwscf.save
+    cd ..
+  done
+  cd ..
+done
