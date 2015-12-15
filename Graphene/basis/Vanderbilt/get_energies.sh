2acae0189386eb5f6d04880b6b56d28c64442eb3
Leandro Seixas <seixasle@gmail.com>
2015-12-16 06:05:55 +0800

    Preparing Graphene PAW and Vanderbilt

---
+++ Graphene/basis/Vanderbilt/get_energies.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for rho in $(seq 4 2 12)
+  do
+  cd rho_$rho
+  for wfc in $(seq 20 5 150)
+    do
+    cd wfc_$wfc
+    energy=`grep ! pw.relax.out | awk '{print $5}'`
+    deltaE=`echo "scale=8; ($energy+23.91095865)*13.60569253*1000/2" | bc`
+    echo $wfc $deltaE >> ../../results_rho_$rho.dat
+    cd ..
+  done
+  cd ..
+done
