fbb53447ea2666f3a321675c408c8eaa5220f524
Leandro Seixas <seixasle@gmail.com>
2015-12-17 13:33:42 +0800

    Graphene/kpoints results

---
+++ Graphene/kpoints/get_energies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for k in $(seq 2 2 20)
+do
+  cd k_$k
+  energy=`grep ! pw.relax.out | awk '{print $5}'`
+  deltaE=`echo "scale=8; ($energy+22.82022773)*13.60569253*1000/2" | bc`
+  echo $k $deltaE
+  cd ..
+done
