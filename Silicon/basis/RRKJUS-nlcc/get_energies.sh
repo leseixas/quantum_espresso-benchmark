e76d759166f53efd2a3211ab112b14a80104dec4
Leandro Seixas <seixasle@gmail.com>
2015-12-20 01:17:09 +0800

    Silicon/basis/RRKJUS-nlcc results

--- Silicon/basis/RRKJUS-nlcc/get_energies.sh
+++ Silicon/basis/RRKJUS-nlcc/get_energies.sh
@@ -4,7 +4,7 @@ for wfc in $(seq 20 5 200)
 do
   cd wfc_$wfc
   energy=`grep ! pw.relax.out | awk '{print $5}'`
-  deltaE=`echo "scale=8; ($energy+15.76237640)*13.60569253*1000/2" | bc`
+  deltaE=`echo "scale=8; ($energy+22.83680804)*13.60569253*1000/2" | bc`
   echo $wfc $deltaE
   cd ..
 done
