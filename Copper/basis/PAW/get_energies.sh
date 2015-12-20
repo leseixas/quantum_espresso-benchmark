c8f5fb03b1ff2f77c794e744d0c962524583259d
Leandro Seixas <seixasle@gmail.com>
2015-12-20 23:42:33 +0800

    Copper/basis/PAW results

--- Copper/basis/PAW/get_energies.sh
+++ Copper/basis/PAW/get_energies.sh
@@ -4,7 +4,7 @@ for wfc in $(seq 20 5 300)
 do
   cd wfc_$wfc
   energy=`grep ! pw.relax.out | awk '{print $5}'`
-  deltaE=`echo "scale=8; ($energy+88.08657006)*13.60569253*1000/2" | bc`
+  deltaE=`echo "scale=8; ($energy+213.21250124)*13.60569253*1000/2" | bc`
   echo $wfc $deltaE
   cd ..
 done
