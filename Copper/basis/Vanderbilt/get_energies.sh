90d3afa58beed2dc391f0dcd2b8796756c326eee
Leandro Seixas <seixasle@gmail.com>
2015-12-21 07:23:59 +0800

    Copper/basis/Vandebilt results

--- Copper/basis/Vanderbilt/get_energies.sh
+++ Copper/basis/Vanderbilt/get_energies.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for wfc in $(seq 20 5 300)
+for wfc in $(seq 20 10 500)
 do
   cd wfc_$wfc
   energy=`grep ! pw.relax.out | awk '{print $5}'`
-  deltaE=`echo "scale=8; ($energy+88.08657006)*13.60569253*1000/2" | bc`
+  deltaE=`echo "scale=8; ($energy+121.48985999)*13.60569253*1000/2" | bc`
   echo $wfc $deltaE
   cd ..
 done
--- Copper/basis/Vanderbilt/run_espresso.sh
+++ Copper/basis/Vanderbilt/run_espresso.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for wfc in $(seq 20 10 400)
+for wfc in $(seq 20 10 500)
   do
   cd wfc_$wfc
   mpiexec pw.x -npool 2 -inp pw.relax.espresso > pw.relax.out
