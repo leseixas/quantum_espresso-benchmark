631ba4deafa09832825892e370a6dc1af91b581d
Leandro Seixas <seixasle@gmail.com>
2015-12-25 03:35:53 +0800

    Diamond/basis/RRKJUS-nlcc results

--- Diamond/basis/RRKJUS-nlcc/get_energies.sh
+++ Diamond/basis/RRKJUS-nlcc/get_energies.sh
@@ -4,7 +4,7 @@ for wfc in $(seq 20 5 150)
   do
   cd wfc_$wfc
   energy=`grep ! pw.relax.out | awk '{print $5}'`
-  deltaE=`echo "scale=8; ($energy+22.77131235)*13.60569253*1000/2" | bc`
-  echo $wfc $deltaE >> ../results.dat
+  deltaE=`echo "scale=8; ($energy+23.89048148)*13.60569253*1000/2" | bc`
+  echo $wfc $deltaE
   cd ..
 done
